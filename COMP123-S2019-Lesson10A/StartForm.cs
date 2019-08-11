e66b8793f91cad274691548547743e4ff33553c3
Tanay Kapoor <kapoortanay15@gmail.com>
2019-08-11 19:49:38 -0400

    Multiple Forms Created

--- COMP123-S2019-Lesson10A/StartForm.cs
+++ COMP123-S2019-Lesson10A/StartForm.cs
@@ -10,11 +10,17 @@ using System.Windows.Forms;
 
 namespace COMP123_S2019_Lesson10A
 {
-    public partial class Form1 : Form
+    public partial class StartForm : Form
     {
-        public Form1()
+        public StartForm()
         {
             InitializeComponent();
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+           Program.Forms[FormName.MAIN_FORM].Show();
+            this.Hide();
         }
     }
 }
